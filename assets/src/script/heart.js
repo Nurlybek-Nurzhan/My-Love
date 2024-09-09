6a52504b026d2165706ae21fcdf6868ef3087df0
Nurlybek Nurzhan <136187061+Nurlybek-Nurzhan@users.noreply.github.com>
2024-09-09 07:38:39 +0500

    OHHHHHHHHHHHHH

--- assets/src/script/heart.js
+++ assets/src/script/heart.js
@@ -1,33 +1,3 @@
-const getRandomNumber = (min, max) => {
-  return Math.round(Math.random() * (max - min) + min);
-};
-
-const loveInterval_1 = setInterval(() => {
-  for (let i = 0; i < 60; i++) {
-    const size = getRandomNumber(2, 4);
-    const x = getRandomNumber(0, window.innerWidth - 120);
-    const y = getRandomNumber(0, window.innerHeight - 120);
-
-    const heart = document.createElement("i");
-    heart.className = "bx bx-heart";
-
-    heart.style.position = "fixed";
-    heart.style.fontSize = `${size}vw`;
-    heart.style.left = `${x}px`;
-    heart.style.top = `${y}px`;
-    heart.style.zIndex = "-99999";
-
-    heart.style.animation = `glow 5s ease-in-out 0s 5 normal none, heart 1s ease-in-out 0s infinite normal none`;
-    document.body.appendChild(heart);
-  }
-  const hearts = document.querySelectorAll(".bx-heart");
-  hearts.forEach((heart) => {
-    setTimeout(() => {
-      heart.remove();
-    }, 5000);
-  });
-}, 5000);
-
 const cuddleButtons = document.querySelectorAll(".cuddle");
 
 cuddleButtons.forEach((button) =>
@@ -41,13 +11,12 @@ cuddleButtons.forEach((button) =>
         heart.style.width = `${size}vw`;
         heart.style.height = `${size}vw`;
         heart.style.left = `${left}%`;
-        heart.style.background = `rgba(255, ${bgColor - 25}, ${bgColor}, 1)`;
+        heart.style.background = `rgba(255, 255, ${bgColor - 125}, 1)`;
         heart.style.animation = `love ${duration}s ease`;
         return heart;
       };
 
       const bgHeart = document.querySelector(".bg_heart");
-      const rNum = randomInRange(1, 40);
       const rSize = randomInRange(2, 4);
       const rLeft = randomInRange(1, 100);
       const rBg = randomInRange(100, 125);
@@ -55,7 +24,7 @@ cuddleButtons.forEach((button) =>
 
       bgHeart.appendChild(createHeart(rSize, rLeft, rBg, rTime));
       bgHeart.appendChild(
-        createHeart(rSize, rLeft + rNum, rBg + 25, rTime + 5)
+        createHeart(rSize, rLeft, rBg + 25, rTime + 5)
       );
 
       const hearts = document.querySelectorAll(".heart");
